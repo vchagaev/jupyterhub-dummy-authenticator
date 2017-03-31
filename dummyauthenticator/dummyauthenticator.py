5909abc7227867295d85947bfaa3462acd24073e
Vladislav Chagaev <vchagaev@gmail.com>
2017-03-31 03:46:06 +0300

    init test token

--- dummyauthenticator/dummyauthenticator.py
+++ dummyauthenticator/dummyauthenticator.py
@@ -4,9 +4,21 @@ from tornado import gen
 
 
 class DummyAuthenticator(Authenticator):
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self._test_token = None
+
     @gen.coroutine
     def authenticate(self, handler, data):
-        if hasattr(data, 'password'):
-            setattr(self, 'test_token', data['password'])
+        self.test_token = data['password']
         return data['username']
+
+    @property
+    def test_token(self):
+        return self._test_token
+
+    @test_token.setter
+    def test_token(self, value):
+        self._test_token = value
+
 
